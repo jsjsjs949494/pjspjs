5768c165c55100775dc89d384c3cf9f16db7d420
jsjsjs949494 <jiseon940511@gmail.com>
2020-05-24 22:43:12 +0900

    5월24일 수정

---
+++ jo/js/js.js
@@ -0,0 +1,19 @@
+$('.aa').click(function(){
+    $('.row').css("opacity","1");
+    $('.row1').css("opacity","0");
+    $('.row2').css("opacity","0");
+})
+
+$('.bb').click(function(){
+    $('.row').css("opacity","0");
+    $('.row1').css("opacity","1");
+    $('.row2').css("opacity","0");
+})
+
+$('.cc').click(function(){
+    $('.row').css("opacity","0");
+    $('.row1').css("opacity","0");
+    $('.row2').css("opacity","1");
+})
+
+
